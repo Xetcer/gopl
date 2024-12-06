0b94f54d9246b9695a4d170bc6f75f232c4184aa
Xetcer <ferdinand_tema@mail.ru>
2024-12-06 15:29:05 +0500

    add fetch utility example

---
+++ chpt01/fetch/fetch.go
@@ -0,0 +1,29 @@
+/*
+осуществляет выборку содержимого по каждому из указанных URL и выводит его как не интерпретированный текст
+go run fetch.go http://www.gopl.io
+*/
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
