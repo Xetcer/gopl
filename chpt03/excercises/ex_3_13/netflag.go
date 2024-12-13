acf6e03f7443c3ed7baba029bdd6d5e54bd0be8d
Xetcer <ferdinand_tema@mail.ru>
2024-12-13 13:55:19 +0500

    add ex 3.13

---
+++ chpt03/excercises/ex_3_13/netflag.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	byte = 1
+	KB   = byte * 1024
+	MB   = KB * 1024
+	GB   = MB * 1024
+	TB   = GB * 1024
+	PB   = TB * 1024
+	EB   = PB * 1024
+	ZB   = EB * 1024
+	YB   = ZB * 1024
+)
