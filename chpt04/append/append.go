a5351c5f666586f7ff390f55972045b04cb18516
Xetcer <ferdinand_tema@mail.ru>
2024-12-16 13:01:37 +0500

    add append example

---
+++ chpt04/append/append.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func appendslice(x []int, y ...int) []int {
+	var z []int
+	zlen := len(x) + len(y)
+	if zlen <= cap(x) {
+		// There is room to expand the slice.
+		z = x[:zlen]
+	} else {
+		// There is insufficient space.
+		// Grow by doubling, for amortized linear complexity.
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	copy(z[len(x):], y)
+	return z
+}
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		// Есть место для расширения среза
+		z = x[:zlen]
+	} else {
+		// нет места для расширения среза
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func main() {
+	var x, y []int
+	for i := 0; i < 10; i++ {
+		y = appendInt(x, i)
+		fmt.Printf("%d  cap=%d\t%v\n", i, cap(y), y)
+		x = y
+	}
+}
