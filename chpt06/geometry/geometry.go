3aec98518d289986e7864d0f7c5b9c490c412696
Xetcer <ferdinand_tema@mail.ru>
2024-12-23 11:15:07 +0500

    add geometry example

---
+++ chpt06/geometry/geometry.go
@@ -0,0 +1,27 @@
+package geometry
+
+import "math"
+
+type Point struct{ X, Y float64 }
+
+// традиционная функция
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// метод типа Point
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type Path []Point
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
