656283688b5d9d0cb22979d616f02bc788df3472
Xetcer <ferdinand_tema@mail.ru>
2024-12-23 08:21:31 +0500

    add title1 example

---
+++ chpt05/title1/title1.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+Программа извлекает документ HTML и выводит его название.
+Функция title проверяет заголовок Content-Туре ответа сервера и возвращает ошибку,
+если документ не является документом HTML.
+
+go run title1.go http://gopl.io
+*/
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	// проверка что загголовок Content-type - HTML
+	// например "text/html; charset=utf-8"
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		resp.Body.Close()
+		return fmt.Errorf("%s имеет тип %s, не text/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return nil
+}
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		if err := title(arg); err != nil {
+			fmt.Fprintf(os.Stderr, "title: %v\n", err)
+		}
+	}
+}
