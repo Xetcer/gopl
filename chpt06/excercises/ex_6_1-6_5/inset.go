af80a02fb81b09de6a39b85800a8a1432c7c2b27
Xetcer <ferdinand_tema@mail.ru>
2024-12-24 10:01:03 +0500

    add excercises 6.1-6.5

--- chpt06/excercises/ex_6_1-6_5/inset.go
+++ chpt06/excercises/ex_6_1-6_5/inset.go
@@ -5,19 +5,22 @@ import (
 	"fmt"
 )
 
+// PLATFORM определяет разрядность платформы 32|64 в зависимости от размера uint на текущей платформе
+const PLATFORM = 32 << (^uint(0) >> 64)
+
 type IntSet struct {
 	words []uint64
 }
 
 // Has указывает, содежит ли множество неотрицательное значениче х
 func (s *IntSet) Has(x int) bool {
-	word, bit := x/64, uint(x%64) // получаем слово и бит от числа X
+	word, bit := x/PLATFORM, uint(x%PLATFORM) // получаем слово и бит от числа X
 	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
 
 // Add добавляет неотрицательное значение x в множество
 func (s *IntSet) Add(x int) {
-	word, bit := x/64, uint(x%64)
+	word, bit := x/PLATFORM, uint(x%PLATFORM)
 	// fmt.Printf("value =%d word=%d bit=%d, slice len=%d\n", x, word, bit, len(s.words))
 	for word >= len(s.words) { // Пока слово не равно длине среза words в s
 		s.words = append(s.words, 0) // добавляем 0 к слову
@@ -36,12 +39,12 @@ func (s *IntSet) AddAll(vals ...int) {
 
 // Remove 6.1 Удаляет x из множества
 func (s *IntSet) Remove(x int) {
-	word, bit := x/64, uint(x%64)
+	word, bit := x/PLATFORM, uint(x%PLATFORM)
 	// fmt.Printf("value =%d word=%d bit=%d, slice len=%d\n", x, word, bit, len(s.words))
 	if word < len(s.words) {
 		// fmt.Println("remove 1 to s.words:", s.words)
-		mask := ^uint64(1 << bit) // Создаем инвертированную маску числа
-		s.words[word] &= mask     // сравниваем с маской текущее число
+		// mask := ^uint64(1 << bit) // Создаем инвертированную маску числа
+		s.words[word] &^= 1 << bit // сравниваем с маской текущее число
 	}
 }
 
@@ -70,6 +73,61 @@ func (s *IntSet) UnionWith(t *IntSet) {
 	}
 }
 
+// IntersectWidth 6.3 делает множество равным пересечению множеств s и t
+func (s *IntSet) IntersectWidth(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// DifferenceWidth 6.3 делает множество s равным разнице множеств s и t
+// Разность множеств — это все элементы, которые содержатся в одном множестве (где вызывается метод),
+// но не содержатся в другом (куда передается аргументом).
+func (s *IntSet) DifferenceWidth(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// SymmetricDifference 6.3 Симметричная разность двух множеств содержит элементы, имеющиеся в одном из множеств, но не в обоих одновременно
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	// c := s.Copy()        // сохраняем s
+	// s.UnionWith(t)       // объединяем s t
+	// c.IntersectWidth(t)  // находим пересечения t и исходного s
+	// s.DifferenceWidth(c) // исключаем из объединения - пересечения
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+
+}
+
+// Elems 6.4 выводим читабельный срез значений множества
+func (s *IntSet) Elems() (result []int) {
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < PLATFORM; j++ {
+			if word&(1<<uint(j)) != 0 {
+				result = append(result, PLATFORM*i+j)
+			}
+		}
+	}
+	return result
+}
+
 // String возвращает множество как строку вида "{1 2 3}".
 func (s *IntSet) String() string {
 	var buf bytes.Buffer
@@ -78,12 +136,12 @@ func (s *IntSet) String() string {
 		if word == 0 {
 			continue
 		}
-		for j := 0; j < 64; j++ {
+		for j := 0; j < PLATFORM; j++ {
 			if word&(1<<uint(j)) != 0 {
 				if buf.Len() > len("{") {
 					buf.WriteByte(' ')
 				}
-				fmt.Fprintf(&buf, "%d", 64*i+j)
+				fmt.Fprintf(&buf, "%d", PLATFORM*i+j)
 			}
 		}
 	}
@@ -95,8 +153,8 @@ func (s *IntSet) String() string {
 func (s *IntSet) Len() int {
 	len := 0
 	for _, word := range s.words {
-		for i := 0; i < 64; i++ {
-			if word&(uint64(1)<<uint64(i)) != 0 {
+		for i := 0; i < PLATFORM; i++ {
+			if word&(1<<i) != 0 {
 				len++
 			}
 		}
@@ -125,8 +183,29 @@ func main() {
 	fmt.Println(x.String())
 	fmt.Println("Len=", x.Len())
 
+	// intersection
+	var y IntSet
+	y.AddAll(4, 8, 15, 16, 23, 42)
+	x.IntersectWidth(&y)
+	fmt.Println("Intersection:", x.String())
+
+	// DifferenceTest
 	x.Clear()
 	fmt.Println("After clear Len=", x.Len())
+	x.DifferenceWidth(&y)
+	fmt.Println("Differense with y:", x.String())
+
+	// SymmetricDifference test
+	x.Clear()
+	y.Clear()
+	x.AddAll(4, 8, 15, 16)
+	y.AddAll(15, 16, 23, 42)
+	x.SymmetricDifference(&y)
+	fmt.Println("Differense with y:", x.String())
+
+	// Elems
+	fmt.Println("Elems of x:", x.Elems())
+
 	// Example_One()
 	// fmt.Println()
 	// Example_Two()
