2fac381744bc3f1680ba9b528e67de52f56f3a86
Xetcer <ferdinand_tema@mail.ru>
2025-01-09 11:12:21 +0500

    add revreb1 example

---
+++ chpt08/reverb1/reverb1.go
@@ -0,0 +1,47 @@
+// go run reverb1.go
+/*
+эхо-сервер может имитировать реверберацию обычного эха, сначала отвечая громко ("HELLO! "),
+затем, после задержки, — умеренно ("Hello! "), а потом — совсем тихо ("hello! ").
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	// Игнорируем потенциальные ошибки input.Err()
+	c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
