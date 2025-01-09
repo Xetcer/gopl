14a7f86be61682c3fe49c86e7d9e128871b2b708
Xetcer <ferdinand_tema@mail.ru>
2025-01-09 15:28:55 +0500

    update info

--- chpt08/pipeline3/pipeline3.go
+++ chpt08/pipeline3/pipeline3.go
@@ -1,7 +1,9 @@
 package main
 
+//  go run pipeline3.go
 import "fmt"
 
+// одноаправленный канал на получение в канал
 func counter(out chan<- int) {
 	for x := 0; x < 100; x++ {
 		out <- x
@@ -9,6 +11,7 @@ func counter(out chan<- int) {
 	close(out)
 }
 
+// два однонаправленных канала на получение в канал и на получение ИЗ канала
 func squarer(out chan<- int, in <-chan int) {
 	for v := range in {
 		out <- v * v
@@ -16,6 +19,7 @@ func squarer(out chan<- int, in <-chan int) {
 	close(out)
 }
 
+// один однонаправленный канала на получение ИЗ канала
 func printer(in <-chan int) {
 	for v := range in {
 		fmt.Println(v)
