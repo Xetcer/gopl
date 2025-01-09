56198663a30aa06b1677e1aefe305a56ef343111
Xetcer <ferdinand_tema@mail.ru>
2025-01-09 09:08:01 +0500

    add netcat1 example

---
+++ chpt08/netcat1/netcat1.go
@@ -0,0 +1,26 @@
+// Netcatl - TCP-клиент только для чтения
+// работает совместно с clock1.go считывает с него время.
+// go run netcat1.go
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
