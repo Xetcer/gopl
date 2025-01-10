208cf0d2681f942af9728f00d42c8b6a456e327a
Xetcer <ferdinand_tema@mail.ru>
2025-01-10 13:37:51 +0500

    add ex 8.7

---
+++ chpt08/excercises/ex_8.7/MirrorLink.go
@@ -0,0 +1,90 @@
+/*
+Exercise 8.7
+Напишите параллельную программу, которая создает локальное зеркало
+веб-сайта, загружая все доступные страницы и записывая их в каталог на локальном
+диске. Выбираться должны только страницы в пределах исходного домена (например,
+golang.org). URL в страницах зеркала должны при необходимости быть изменены
+таким образом, чтобы они ссылались на зеркальную страницу, а не на оригинал.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+
+	"gopl/chpt05/links"
+)
+
+var (
+	wg   = &sync.WaitGroup{}
+	base = flag.String("u", "https://htmlacademy.ru/blog/html/free-html-template", "base url to crawl")
+)
+
+func main() {
+	flag.Parse()
+
+	for _, url := range crawl(*base) {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			download(*base, url)
+		}(url)
+	}
+	done := make(chan struct{})
+
+	go func() {
+		wg.Wait()
+		done <- struct{}{}
+	}()
+	<-done
+}
+
+func download(base, url string) {
+	fmt.Println("Try to download page:", url)
+	if !strings.HasPrefix(url, base) || strings.Contains(url, "#") {
+		return
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	dir := strings.TrimPrefix(strings.TrimPrefix(url, "http://"), "https://")
+	fmt.Println("dir is:", dir)
+	if err := os.MkdirAll(dir, 0755); err != nil && !os.IsExist(err) {
+		log.Println(err)
+	}
+	ext := "index.html"
+	filename := dir + ext
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Println(err)
+	}
+	return list
+}
---
+++ chpt08/excercises/ex_8.7/go.mod
@@ -0,0 +1,9 @@
+module gopl/chpt08/MirrorLink
+
+go 1.23.4
+
+replace gopl/chpt05/links => ../../../chpt05/links
+
+require gopl/chpt05/links v0.0.0-00010101000000-000000000000
+
+require golang.org/x/net v0.33.0 // indirect
