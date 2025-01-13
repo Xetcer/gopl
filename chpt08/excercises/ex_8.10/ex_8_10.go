ddb13db223f8f73b94d59cb4cabffa5183fb216c
Xetcer <ferdinand_tema@mail.ru>
2025-01-13 09:19:23 +0500

    add ex 8.10

---
+++ chpt08/excercises/ex_8.10/ex_8_10.go
@@ -0,0 +1,65 @@
+/*
+Exercise 8.10
+Запросы HTTP могут быть отменены с помощью закрытия
+необязательного канала Cancel в структуре http.Request. Измените веб-сканер из
+раздела 8.6 так, чтобы он поддерживал отмену. Указание. Функция http.Get не
+позволяет настроить Request. Вместо этого создайте запрос с использованием
+http.NewRequest, установите его поле Cancel и выполните запрос с помощью вызова
+http.DefaultClient.Do(req).
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopl/chpt08/excercises/ex_8.10/links"
+)
+
+func crawl(url string, cancelled <-chan struct{}) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url, cancelled)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)  //  Список URL, могут быть дубли.
+	unseenLinks := make(chan string) // Удаление дублей.
+
+	// Добавление в список аргументов командной строки
+	go func() { worklist <- os.Args[1:] }()
+
+	cancelled := make(chan struct{})
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(cancelled)
+	}()
+
+	// Создание 20 горутин сканирования для выборки всех непросмотренных ссылок.
+	for i := 0; i < 20; i++ {
+		go func() {
+			for link := range unseenLinks {
+				foundLinks := crawl(link, cancelled)
+				go func() { worklist <- foundLinks }()
+
+			}
+		}()
+	}
+
+	// Главная горутина удаляет дубликаты из списка и отправляет непросмотренные ссылки сканерам.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				unseenLinks <- link
+			}
+		}
+	}
+}
---
+++ chpt08/excercises/ex_8.10/links/go.mod
@@ -0,0 +1,5 @@
+module gopl/chpt08/excercises/ex_8.10/links
+
+go 1.23.4
+
+require golang.org/x/net v0.34.0
